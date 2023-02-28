c12c9b73d3576f646de22f9ab36f5f034aa7c6e8
Tretyakov Ivan <timmitarh@mail.ru>
2023-02-28 20:50:08 +0300

    Completed task #10

---
+++ Task_10/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+
+int userNumber;
+while (true)
+{
+    Console.Write("Введите целое трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out userNumber))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+int result = userNumber / 10 % 10;
+
+Console.WriteLine($"{userNumber} -> {result}");
