6232f07ddcb3cfbea6de4323df7c3b08fc1acafc
Tretyakov Ivan <timmitarh@mail.ru>
2023-03-01 10:40:33 +0300

    Completed task #13

---
+++ Task_11/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+
+int userNumber;
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    if (int.TryParse(Console.ReadLine(), out userNumber))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+int userNumberAbs = Math.Abs(userNumber);
+
+if (userNumberAbs < 100)
+{
+    Console.WriteLine($"{userNumber} -> третьей цифры нет");
+}
+else
+{
+    while (userNumberAbs > 999)
+    {
+        userNumberAbs = userNumberAbs / 10;
+    }
+    int result = userNumberAbs % 10;
+    
+    Console.WriteLine($"{userNumber} -> {result}");
+}
